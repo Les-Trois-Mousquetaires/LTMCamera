40043e5433e0ed432a399d0f2d40007d3d42f1e3
coenen <coenen@aliyun.com>
2020-09-27 19:49:11 +0800

    创建

---
+++ LTMCamera/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LTMCamera
+//
+//  Created by 柯南 on 2020/9/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LTMCamera/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LTMCamera
+//
+//  Created by 柯南 on 2020/9/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
